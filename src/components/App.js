38fee8f44b91ca1077aecc0387670df5c6d1f9f2
James Carrier <jic191278@gmail.com>
2024-03-28 01:34:24 +0000

    clean file structure and sort linter rules

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/app.css';
+
+function App() {
+  return (
+    <div className="app">
+      <h1>App Component</h1>
+    </div>
+  );
+}
+
+export default App;
